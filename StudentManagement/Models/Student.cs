44ce965eb5ec487d3455e7e538ce5b9862b0cbc9
sudhakar <sudhakar@btbp.org>
2025-07-17 07:35:35 +0530

    Add project files.

---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentManagement.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ StudentManagement/UserControls/StudentForm.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentManagement.UserControls
+{
+    /// <summary>
+    /// Interaction logic for StudentForm.xaml
+    /// </summary>
+    public partial class StudentForm : UserControl
+    {
+        public StudentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
